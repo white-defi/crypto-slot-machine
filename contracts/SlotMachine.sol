ac091f580b813949e9b756d6c7a882a634e188f8
shendel <im_shendel@inbox.ru>
2023-01-26 11:30:33 +0300

    contract init

---
+++ contracts/SlotMachine.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract SlotMachine is Ownable {
+    
+    constructor(
+    ) {
+        
+    }
+}
